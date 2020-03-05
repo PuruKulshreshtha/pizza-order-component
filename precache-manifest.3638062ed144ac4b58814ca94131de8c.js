37d3230eb9d8e29bc567a4239f16ac5819c6aba4
manish-dalal <manish.dalal@daffodilsw.com>
2020-03-05 09:09:01 +0530

    Updates

---
+++ precache-manifest.3638062ed144ac4b58814ca94131de8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8773d91a7fcc332064dc2f9dcefb7a2d",
+    "url": "/pizza-order-component/index.html"
+  },
+  {
+    "revision": "bc22995fd51328d69392",
+    "url": "/pizza-order-component/static/css/main.54636aed.chunk.css"
+  },
+  {
+    "revision": "f0cacdba252193ae5239",
+    "url": "/pizza-order-component/static/js/2.8286b949.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pizza-order-component/static/js/2.8286b949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc22995fd51328d69392",
+    "url": "/pizza-order-component/static/js/main.f34aef17.chunk.js"
+  },
+  {
+    "revision": "64be50c6d70a9a75f907",
+    "url": "/pizza-order-component/static/js/runtime-main.aa37ab21.js"
+  }
+]);
